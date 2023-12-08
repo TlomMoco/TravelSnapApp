38ab706def48856840acd2e200ea6a59ad9f7a32
Barua98 <ronyb98@gmail.com>
2023-12-08 15:18:39 +0100

    Added icons to the tabs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,20 +3,59 @@ import { View, Text } from "react-native";
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import GeoLocationPage from "../pages/GeoLocationPage";
 import HomePage from "../pages/HomePage";
-import CameraPage from "../pages/CameraPage"
-import SettingsPage from "../pages/SettingsPage"
+import CameraPage from "../pages/CameraPage";
+import SettingsPage from "../pages/SettingsPage";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+/*
+    Referanse:
+    Material Bottom Tabs Navigator API 
+    https://reactnavigation.org/docs/material-bottom-tab-navig
+
+    Referanse: 
+    Icons 
+    https://docs.expo.dev/guides/icons/#properties
+*/
 
 const Tab = createMaterialBottomTabNavigator();
 
+type TabIconProps = {
+    focused: boolean;
+    color: string;
+    size: number;
+  };
 
+type IconName = React.ComponentProps<typeof Ionicons>['name'];
+
+const createTabOptions = (iconName: IconName) => ({
+    tabBarIcon: () => (
+      <Ionicons name= {iconName} color={"black"} size={26} />
+    ),
+  });
 
 const TabsView = () => {
     return(
-       <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomePage}></Tab.Screen>
-            <Tab.Screen name="Geo location" component={GeoLocationPage}></Tab.Screen>
-            <Tab.Screen name="Camera" component={CameraPage}></Tab.Screen>
-            <Tab.Screen name="Settings" component={SettingsPage}></Tab.Screen>
+       <Tab.Navigator barStyle={{ backgroundColor: "#f97316"}}>
+            <Tab.Screen 
+                name="Home" 
+                component={HomePage}
+                options= {createTabOptions("home-outline")}
+            ></Tab.Screen>
+            <Tab.Screen   
+                name="Geo location"
+                component={GeoLocationPage}
+                options={createTabOptions("globe-outline")}
+            ></Tab.Screen>
+            <Tab.Screen 
+                name="Camera"
+                component={CameraPage}
+                options={createTabOptions("camera-outline")}
+            ></Tab.Screen>
+            <Tab.Screen 
+                name="Settings" 
+                component={SettingsPage}
+                options={createTabOptions("settings-outline")}
+            ></Tab.Screen>
        </Tab.Navigator>
     );
 };
