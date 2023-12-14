ffa82d1c3376162691bcfb753fd9b09a27b80a6c
TlomMoco <trym.marco@gmail.com>
2023-12-14 22:10:02 +0100

    big fix

--- src/pages/ImageDescriptionPage/index.tsx
+++ src/pages/ImageDescriptionPage/index.tsx
@@ -1,29 +1,83 @@
-
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Image, ImageBackground, TouchableOpacity } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../model/data';
 import { UseImageContext } from '../../providers/TravelSnapContextProvider';
-
 import { ImageUpload } from '../../components/CameraComponents/ImageUpload';
 import { ActivityIndicator } from 'react-native';
-import *  as ImagePicker from "expo-image-picker";
-
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ImageDescriptionPage'>;
 
+const ImageDescriptionPage: React.FC<Props> = () => {
+
+    // Get context
+    const context = UseImageContext();
+
+    // Give navigation and route
+    const { params } = useRoute<Props['route']>();
+    const navigation = useNavigation();
+    const { imageId, imageUrl } = params;
+    const localUri = params.imageUrl; 
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        context?.getCurrentDescription();
+    },[]) 
+
+    const handleSubmit = async () => {
+      setLoading(true);
+        try {
+          const downloadURL = await ImageUpload(localUri, imageId); 
 
           context?.setCurrentImage({
-            imageUri: imageUpload,
+            imageUri: downloadURL,
             uniqueId: imageId,
           });
 
+          context?.setFirestoreValues(context.description);
 
+          navigation.goBack();
+        } catch (error) {
+          console.error('Error writing document to Firebase Storage:', error);
+        }finally{
+          setLoading(false);
+        }
+      };
+
+
+    return (
+      <ImageBackground
+      source={require('../../assets/images/BackgroundForestDescription.png')}
+      resizeMode="cover"
+      className="flex-1"
+    >
+      {loading ? (
+        <View className="flex-1 justify-center items-center">
+          <ActivityIndicator size="large" color="black" />
+        </View>
+      ) : (
+        <View className="flex-1 justify-center items-center px-6">
+          <View className="w-full items-center mt-10 bg-white/80 rounded-lg">
+            <Text className="text-2xl m-5">Image Caption</Text>
+            <Image source={{uri: context?.currentImage?.imageUri}} style={{width: 200, height: 200, borderRadius: 5}}/>
+            <View className="w-full px-8 p-3">
+              <TextInput
+                className="mb-4 p-4 h-12 border border-gray-300 rounded-lg"
+                placeholder="Enter description"
+                onChangeText={(text) => {context?.setDescription(text)}}
+              />
+
+              <TouchableOpacity
+                onPress={handleSubmit}className="mb-4 bg-orange-500 py-2 rounded-lg flex items-center justify-center">
+                <Text className="text-white text-lg">Submit</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-      </View>
-    )}
-  </ImageBackground>
-  );
+      )}
+    </ImageBackground>
+    );
 };
 
 export default ImageDescriptionPage;
