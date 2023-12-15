8df544295f44db7599eb5d1f5b8370001445edc9
Barua98 <ronyb98@gmail.com>
2023-12-15 03:36:29 +0100

    Added spinner to the location

--- src/pages/ImageDescriptionPage/index.tsx
+++ src/pages/ImageDescriptionPage/index.tsx
@@ -31,6 +31,7 @@ const ImageDescriptionPage: React.FC<Props> = () => {
   const {imageId, imageUrl} = params;
   const localUri = params.imageUrl; 
   const [loading, setLoading] = useState(false);
+  const [locationLoading, setLocationLoading] = useState(false);
   const [currentImage, setCurrentImage] = useState<string | null>(imageUrl);
 
   const handleSubmit = async () => {
@@ -99,6 +100,7 @@ const ImageDescriptionPage: React.FC<Props> = () => {
   }, [context, imageId]);
 
   const getLocationAccess = async () => {
+    setLocationLoading(true);
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== 'granted') {
       Alert.alert("Permission Required", "This app needs permission to access your location.");
@@ -108,50 +110,65 @@ const ImageDescriptionPage: React.FC<Props> = () => {
     context?.setLocation(location);
     console.log("Location:", location);
     context?.setLocation(location);
+    setLocationLoading(false);
   }
-
-  return (
-    <ImageBackground
-    source={require('../../assets/images/BackgroundForestDescription.png')}
-    resizeMode="cover"
-    className="flex-1"
-  >
-    {loading ? (
-      <View className="flex-1 justify-center items-center">
-        <ActivityIndicator size="large" color="black" />
-      </View>
-    ) : (
-      <ScrollView contentContainerStyle={{flexGrow: 1, justifyContent: "center"}}>
-        <View className="flex-1 justify-center items-center px-6">
-          <View className="w-full items-center mt-10 bg-white/80 rounded-lg">
-            <Text className="text-2xl m-5">Add details</Text>
-            {currentImage && (
-                <Image source={{ uri: currentImage }} style={{ width: 300, height: 350, borderRadius: 5 }}/>
-              )}
-            <View className="w-full px-8 p-3">
-              <TouchableOpacity
-                  onPress={uploadFromGallery}className="mb-4 bg-orange-500 py-2 rounded-lg flex items-center justify-center">
-                  <Text className="text-white text-lg">Upload from gallery</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                  onPress={getLocationAccess}className="mb-4 bg-orange-500 py-2 rounded-lg flex items-center justify-center">
-                  <Text className="text-white text-lg">Set location</Text>
-              </TouchableOpacity>
-              <TextInput
-                  className="mb-4 p-4 h-12 border border-gray-300 rounded-lg"
-                  placeholder="Enter description"
-                  onChangeText={(text) => context?.setDescription(text)}
-              />
-              <TouchableOpacity
-                  onPress={handleSubmit}className="mb-4 bg-orange-500 py-2 rounded-lg flex items-center justify-center">
-                  <Text className="text-white text-lg">Submit</Text>
-              </TouchableOpacity>
-            </View>
+    return (
+      <ImageBackground
+        source={require('../../assets/images/BackgroundForestDescription.png')}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        {loading ? (
+          <View className="flex-1 justify-center items-center">
+            <ActivityIndicator size="large" color="black" />
           </View>
-        </View>
-      </ScrollView>
-    )}
-  </ImageBackground>
-  );
+        ) : (
+          <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}>
+            <View className="flex-1 justify-center items-center px-6">
+              <View className="w-full items-center mt-10 bg-white/80 rounded-lg">
+                <Text className="text-2xl m-5">Add details</Text>
+                {currentImage && (
+                  <Image source={{ uri: currentImage }} style={{ width: 300, height: 350, borderRadius: 5 }} />
+                )}
+                <View className="w-full px-8 p-3">
+                  <TouchableOpacity
+                    onPress={uploadFromGallery}
+                    className="mb-4 bg-orange-500 py-2 rounded-lg flex items-center justify-center"
+                  >
+                    <Text className="text-white text-lg">Upload from gallery</Text>
+                  </TouchableOpacity>
+                  {locationLoading ? (
+                    <View className="mb-4 flex items-center justify-center">
+                      <ActivityIndicator size="small" color="black" />
+                      <Text className="text-black text-lg">Setting location...</Text>
+                    </View>
+                  ) : (
+                    <>
+                      <TouchableOpacity
+                        onPress={getLocationAccess}
+                        className="mb-4 bg-orange-500 py-2 rounded-lg flex items-center justify-center"
+                      >
+                        <Text className="text-white text-lg">Set location</Text>
+                      </TouchableOpacity>
+                      <TextInput
+                        className="mb-4 p-4 h-12 border border-gray-300 rounded-lg"
+                        placeholder="Enter description"
+                        onChangeText={(text) => context?.setDescription(text)}
+                      />
+                      <TouchableOpacity
+                        onPress={handleSubmit}
+                        className="mb-4 bg-orange-500 py-2 rounded-lg flex items-center justify-center"
+                      >
+                        <Text className="text-white text-lg">Submit</Text>
+                      </TouchableOpacity>
+                    </>
+                  )}
+                </View>
+              </View>
+            </View>
+          </ScrollView>
+        )}
+      </ImageBackground>
+    );
 };
 export default ImageDescriptionPage;
